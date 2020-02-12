bbe9ad6e3881d9ade7111b29fbe47082c6c42090
Viktor Luft <viktor.luft@freiheit.com>
2020-02-12 21:12:19 +0100

    Allow custom colors for calendar events

---
+++ precache-manifest.1d95cf2f8fc720ad223585ad4da58264.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e7225a7fa533d5aeaa17aeea062311",
+    "url": "/board.jpg"
+  },
+  {
+    "revision": "72d35dcfd0079eb9e27525aec2236c99",
+    "url": "/events.json"
+  },
+  {
+    "revision": "aa42416f0715c9225e0726d319399fc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e5f426ce65fd2b91f1b69b50ac86163",
+    "url": "/light.jpg"
+  },
+  {
+    "revision": "090dd71c4e9a697fbcc540bc8f54c55b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "dd6bc38bce04f827966c3e8ac089c4b1",
+    "url": "/nb-logo-128.png"
+  },
+  {
+    "revision": "bc7b52609221441b45ffd33ff9f5753e",
+    "url": "/nb-logo-144.png"
+  },
+  {
+    "revision": "1e642a39bac9c9692432d08a6337ffa5",
+    "url": "/nb-logo-152.png"
+  },
+  {
+    "revision": "432ddf96964553a65b4527fdd321ae29",
+    "url": "/nb-logo-192.png"
+  },
+  {
+    "revision": "140f5b5516b47844bf63fcbc7d73be0c",
+    "url": "/nb-logo-32.png"
+  },
+  {
+    "revision": "0c27d2f878c5204bc8c52552eb259169",
+    "url": "/nb-logo-512.png"
+  },
+  {
+    "revision": "a775af271e8685b2219088c48524f18d",
+    "url": "/nb-logo.svg"
+  },
+  {
+    "revision": "0e6ad0774c2cd31d7fb628aa15599f6d",
+    "url": "/plant.jpg"
+  },
+  {
+    "revision": "3472e30371ffcb0448712096e3162ac2",
+    "url": "/reserve.jpg"
+  },
+  {
+    "revision": "9bc37258a48282b6864ba3d0b755d037",
+    "url": "/reviews.json"
+  },
+  {
+    "revision": "69fb299f7f5061f140f6b1e62ce0fe16",
+    "url": "/share.jpg"
+  },
+  {
+    "revision": "bd91df28eea6cd05754f",
+    "url": "/static/css/main.3ff7eb21.chunk.css"
+  },
+  {
+    "revision": "217533751f7be932c086",
+    "url": "/static/js/2.74fcf29b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.74fcf29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd91df28eea6cd05754f",
+    "url": "/static/js/main.1446f8f8.chunk.js"
+  },
+  {
+    "revision": "d51a055786ea4fe4dc68",
+    "url": "/static/js/runtime-main.d3cb9203.js"
+  },
+  {
+    "revision": "335e13ccad72174c273b514d9d49cb7e",
+    "url": "/sweep.jpg"
+  },
+  {
+    "revision": "48a3d7dc472b82a754c4911935eef540",
+    "url": "/trash.jpg"
+  },
+  {
+    "revision": "f0190ab2c8c4e0c690b6fbf6e862cf4d",
+    "url": "/window.jpg"
+  }
+]);
