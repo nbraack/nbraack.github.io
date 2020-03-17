11b32d434b73028f42bf6afc0c77841b80dbe4dd
Viktor Luft <viktor.luft@freiheit.com>
2020-03-17 07:50:19 +0100

    Aufgaben wieder entfernt

---
+++ precache-manifest.4d0ed7534c3949d75631eb5ba459f5ab.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e7225a7fa533d5aeaa17aeea062311",
+    "url": "/board.jpg"
+  },
+  {
+    "revision": "137e9cdb61a1ee2fe26d6533e3952f3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e5f426ce65fd2b91f1b69b50ac86163",
+    "url": "/light.jpg"
+  },
+  {
+    "revision": "090dd71c4e9a697fbcc540bc8f54c55b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "dd6bc38bce04f827966c3e8ac089c4b1",
+    "url": "/nb-logo-128.png"
+  },
+  {
+    "revision": "bc7b52609221441b45ffd33ff9f5753e",
+    "url": "/nb-logo-144.png"
+  },
+  {
+    "revision": "1e642a39bac9c9692432d08a6337ffa5",
+    "url": "/nb-logo-152.png"
+  },
+  {
+    "revision": "432ddf96964553a65b4527fdd321ae29",
+    "url": "/nb-logo-192.png"
+  },
+  {
+    "revision": "140f5b5516b47844bf63fcbc7d73be0c",
+    "url": "/nb-logo-32.png"
+  },
+  {
+    "revision": "0c27d2f878c5204bc8c52552eb259169",
+    "url": "/nb-logo-512.png"
+  },
+  {
+    "revision": "a775af271e8685b2219088c48524f18d",
+    "url": "/nb-logo.svg"
+  },
+  {
+    "revision": "0e6ad0774c2cd31d7fb628aa15599f6d",
+    "url": "/plant.jpg"
+  },
+  {
+    "revision": "3472e30371ffcb0448712096e3162ac2",
+    "url": "/reserve.jpg"
+  },
+  {
+    "revision": "69fb299f7f5061f140f6b1e62ce0fe16",
+    "url": "/share.jpg"
+  },
+  {
+    "revision": "0b4b691cc6e2d2c7853a",
+    "url": "/static/css/main.07692513.chunk.css"
+  },
+  {
+    "revision": "217533751f7be932c086",
+    "url": "/static/js/2.74fcf29b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.74fcf29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4b691cc6e2d2c7853a",
+    "url": "/static/js/main.f920179d.chunk.js"
+  },
+  {
+    "revision": "d51a055786ea4fe4dc68",
+    "url": "/static/js/runtime-main.d3cb9203.js"
+  },
+  {
+    "revision": "335e13ccad72174c273b514d9d49cb7e",
+    "url": "/sweep.jpg"
+  },
+  {
+    "revision": "48a3d7dc472b82a754c4911935eef540",
+    "url": "/trash.jpg"
+  },
+  {
+    "revision": "f0190ab2c8c4e0c690b6fbf6e862cf4d",
+    "url": "/window.jpg"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.d858e849b876e67fbf2733b1a4a4ad28.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.4d0ed7534c3949d75631eb5ba459f5ab.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // See https://developers.google.com/web/tools/workbox/guides/configure-workbox
 workbox.setConfig({ debug: false });
