f2f8cbcc9533f043b598b394f0403902c5b3fb28
Viktor Luft <viktor.luft@freiheit.com>
2021-08-08 19:03:47 +0200

    Fix SW issue

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'nbraack-4c1cf18c-3948-4d27-b61b-3dd9951d974c';
+const CACHE_NAME = 'nbraack-fd154202-be67-4789-a927-e707727f37e3';
 const STATIC_FILES = ['/','/index.html','/static/css/main.55079bcc.chunk.css','/static/js/2.4bae4dea.chunk.js','/static/js/2.4bae4dea.chunk.js.LICENSE.txt','/static/js/main.6990131f.chunk.js','/static/js/runtime-main.3243a27a.js','/static/media/chalk-board.67e07c37.otf'];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches()
+        caches
             .open(CACHE_NAME)
             .then(cache => cache.addAll(STATIC_FILES))
             .then(() => self.skipWaiting())
@@ -11,12 +11,12 @@ self.addEventListener('install', (event) => {
 });
 self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches()
+        caches
             .keys()
             .then(keyList =>
                 Promise.all(
                     keyList.map(key => {
-                        if (key !== CACHE_NAME) return caches().delete(key);
+                        if (key !== CACHE_NAME) return caches.delete(key);
                     })
                 )
             )
